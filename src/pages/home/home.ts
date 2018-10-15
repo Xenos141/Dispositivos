d006446831a4eeb2f2c48998adf4547d28152162
MikeElMagnifico <al3201@cedi.local>
2018-10-15 08:34:29 -0500

    Subir el archivo

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,37 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { BocinaPage } from '../bocina/bocina';
+import { TvPage } from '../tv/tv';
+import { LamparaPage } from '../lampara/lampara';
+import { SmartphonePage } from '../smartphone/smartphone';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+bocina = BocinaPage;
+tv = TvPage;
+lampara = LamparaPage;
+tel = SmartphonePage;
   constructor(public navCtrl: NavController) {
 
   }
 
+  Bocina()
+  {
+    this.navCtrl.push(this.bocina);
+  }
+  TV()
+  {
+    this.navCtrl.push(this.tv);
+  }
+  Lampara()
+  {
+    this.navCtrl.push(this.lampara);
+  }
+  Smartphone()
+  {
+    this.navCtrl.push(this.tel);
+  }
 }
